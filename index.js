904db0720ac0cd45dc9620992aefc9b71b60e212
sumitsharma17361-ui <sumitsharma17361@gmail.com>
2026-07-30 11:36:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -251,3 +251,4 @@ app.get('/api/attendance/all/:requesterRollNo', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+                    
