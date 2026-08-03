189bc47ee358a14eb6ba467eeeb7804bd4bbe2c6
sumitsharma17361-ui <sumitsharma17361@gmail.com>
2026-08-03 15:04:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -341,4 +341,3 @@ app.delete('/api/attendance/delete/:id/:requesterRollNo', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-        
