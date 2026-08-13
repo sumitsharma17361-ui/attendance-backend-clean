3e2853987c3e675158910c2391b5da088a721d2c
sumitsharma17361-ui <sumitsharma17361@gmail.com>
2026-08-13 14:22:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const MONGO_URI = process.env.MONGO_URI;
 const JWT_SECRET = process.env.JWT_SECRET || "super_secret_key_123";
 const COLLEGE_LAT = 28.4509370;
 const COLLEGE_LNG = 76.7688120;
-const COLLEGE_RADIUS = 50;
+const COLLEGE_RADIUS = 100;
 const SEMESTER_START = new Date(2026, 6, 15);
 const SEMESTER_END = new Date(2026, 11, 31);
 
